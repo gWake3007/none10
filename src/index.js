d8c31b93b9aef51959c26bdae00d73d3b2626f33
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-03-07 23:10:56 +0100

    1

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_xMnfgK6ktKZ8DHw1y5QsGeWrKw4fGeuH16i2bt4U3syOORaIkM5Q3anmRy0MPesu';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  catInfo: document.querySelector('.cat-info'),
+};
+
