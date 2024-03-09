a8b2fb2f79d4ab4e54b5862aa020cd485fdc652b
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-03-09 13:47:20 +0100

    1

--- src/index.js
+++ src/index.js
@@ -11,6 +11,9 @@ const refs = {
   catInfo: document.querySelector('.cat-info'),
 };
 
+fetchBreeds();
+refs.select.addEventListener('change', changeSelect);
+
 async function fetchBreeds() {
   await axios
     .get('https://api.thecatapi.com/v1/breeds')
@@ -26,8 +29,38 @@ async function fetchBreeds() {
     });
 }
 
-fetchBreeds();
-
 async function fetchCatByBreed(breedId) {
-    axios.get(``)
+  const URL = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
+  await axios
+    .get(`${URL}${breedId}`)
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+async function changeSelect(event) {
+  const breedId = event.target.value;
+  try {
+    const posts = await fetchCatByBreed(breedId);
+    renderCatInfo(posts);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function renderCatInfo(arr) {
+  const markup = arr
+    .map(({ description, name, temperament, image: { url } }) => {
+      return `<li class="cat-cart">
+<img class="cat-img" src="${url}" alt="${name}" />
+<h2 class="cat-name">${name}</h2>
+<p class="cat-text">${description}</p>
+<h4 class="title-temperament">Temperament:<span class="temperament">${temperament}</span></h4>
+</li>`;
+    })
+    .join('');
+  refs.catInfo.innerHTML = markup;
 }
