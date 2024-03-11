b5f12044dd3f0a1a470fbc8b7b62fe0f00d8a5b6
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-03-11 11:17:19 +0100

    1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1/';
 axios.defaults.headers.common['x-api-key'] =
   'live_xMnfgK6ktKZ8DHw1y5QsGeWrKw4fGeuH16i2bt4U3syOORaIkM5Q3anmRy0MPesu';
 import Notiflix from 'notiflix';
@@ -11,58 +12,80 @@ const refs = {
   catInfo: document.querySelector('.cat-info'),
 };
 
+const slim = new SlimSelect({
+  select: '#multiple',
+  settings: {
+    // Below are a list of optional fields
+    // their values are the defaults
+    disabled: false,
+    alwaysOpen: false,
+    showSearch: true,
+    searchPlaceholder: 'Search',
+    searchText: 'No Results',
+    searchingText: 'Searching...',
+    searchHighlight: false,
+    closeOnSelect: true,
+    contentLocation: document.body,
+    contentPosition: 'absolute',
+    openPosition: 'auto', // options: auto, up, down
+    placeholderText: 'Select Value',
+    allowDeselect: false,
+    hideSelected: false,
+    showOptionTooltips: false,
+    minSelected: 0,
+    maxSelected: 1000,
+    timeoutDelay: 200,
+    maxValuesShown: 20,
+    maxValuesMessage: '{number} selected',
+  },
+});
+
+const errorText = refs.error.textContent;
+
 fetchBreeds();
 refs.select.addEventListener('change', changeSelect);
 
 async function fetchBreeds() {
   await axios
-    .get('https://api.thecatapi.com/v1/breeds')
+    .get('breeds')
     .then(response => {
-      const option = response.data
-        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
-        .join('');
-      refs.select.insertAdjacentHTML('afterbegin', option);
+      const options = response.data.map(({ id, name }) => ({ value: id, text: name }));
+      slim.setData(options);
     })
     .catch(error => {
       console.error('Error:', error);
+      Notiflix.Notify.failure(errorText);
     });
 }
 
 async function fetchCatByBreed(breedId) {
-  const URL = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
-  await axios
-    .get(`${URL}${breedId}`)
-    .then(data => {
-        // console.log(data.data);
-        return data;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+  const URL = 'images/search?breed_ids=';
+  const response = await axios.get(`${URL}${breedId}`);
+  return response.data;
 }
 
 async function changeSelect(event) {
   const breedId = event.target.value;
   try {
     const posts = await fetchCatByBreed(breedId);
-    console.log(fetchCatByBreed(breedId));
     renderCatInfo(posts);
   } catch (error) {
     console.log(error);
+    Notiflix.Notify.failure(errorText);
   }
 }
 
-function renderCatInfo(arr) {
-  const markup = arr
-    .flatMap(({description, name, temperament, vetstreet_url}) => {
-      return `<li class="cat-cart">
-<img class="cat-img" src="${vetstreet_url}" alt="${name}" />
+function renderCatInfo(data) {
+  const url = data[0].url;
+  const description = data[0].breeds[0].description;
+  const temperament = data[0].breeds[0].temperament;
+  const name = data[0].breeds[0].name;
+  const markup = `<li class="cat-cart">
+<img class="cat-img" src="${url}" alt="${name}" width="350px"/>
 <h2 class="cat-name">${name}</h2>
 <p class="cat-text">${description}</p>
 <h4 class="title-temperament">Temperament:<span class="temperament">${temperament}</span></h4>
 </li>`;
-    })
-    .join('');
-    console.log(markup);
+  console.log(markup);
   refs.catInfo.innerHTML = markup;
 }
